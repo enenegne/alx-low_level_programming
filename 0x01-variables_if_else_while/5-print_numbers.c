ce92c07f4e49d8de345837c6cd6c8ad04d887d68
enenegne <mikiasabiyy@gmail.com>
2021-10-17 11:57:14 -0400

    a program that prints numbers from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints base number 10 0 - 9.
+ *
+ * Return: 0 if sucessesful else another number.
+ */
+
+int main(void)
+{
+	printf("123456789\n");
+	return (0);
+}
