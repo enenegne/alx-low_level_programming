c51ec317e5028708d578a42fb48e0a34d67bd90e
enenegne <mikiasabiyy@gmail.com>
2021-10-17 17:38:02 -0400

    a program that updates the value of a var to 98

--- 0x05-pointers_arrays_strings/0-reser_to_98.c
+++ 0x05-pointers_arrays_strings/0-reser_to_98.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+
 /**
- * reset_to_98 - reset the value of the variable that n pointes to to 98. 
+ * reset_to_98 - reset the value of the variable that n pointes to to 98.
+ * @n: pointer to an int to dereference.
  *
  * Return: void;
  */
@@ -8,6 +10,5 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return;
 }
 
