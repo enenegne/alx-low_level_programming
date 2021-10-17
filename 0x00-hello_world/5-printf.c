5f1d8c6311eaeff9268de029ad1210567ca6070d
enenegne <mikiasabiyy@gmail.com>
2021-10-17 06:35:05 -0400

    a program that prints a text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints a string.
+ *
+ * Return: 0 if successful else another number.
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
