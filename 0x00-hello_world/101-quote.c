0907398b14bb65d0cae32bb21da5ed060df3621c
enenegne <mikiasabiyy@gmail.com>
2021-10-17 10:25:36 -0400

    a program that prints a a string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	char tp[] = "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n";
+	char tp[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int i;
 	int l = strlen(tp);
 
@@ -19,5 +19,5 @@ int main(void)
 		putchar(tp[i]);
 		i++;
 	}
-	return (0);
+	return (1);
 }
