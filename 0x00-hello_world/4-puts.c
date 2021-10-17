97da861043e8fe0f88ae74e2c9245ace24fbbd98
enenegne <mikiasabiyy@gmail.com>
2021-10-17 06:19:52 -0400

    a program that prints a text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
+ * main - a program that prints a text.
  *
- *
- *
- *
+ * Return: 0 if sucessesful else another number.
  */
 
 int main(void)
 {
 	char tp[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(tp);
 	return (0);
 }
