65358468bacb63d55de9b75c65ff4e7a38225ee0
enenegne <mikiasabiyy@gmail.com>
2021-10-18 09:27:58 -0400

    correction

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+
+#include <elf.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stddef.h>
+
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+
 #endif
